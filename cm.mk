1dc8bc8a4e83c192f7607af771f96e849bd47ec5
XileForce <rdaguiar360@gmail.com>
2015-01-11 18:47:25 -0800

    Make it Onyx
    Change-Id: Id3f14aa4024703e058d507579c09c23fd8474258

--- cm.mk
+++ cm.mk
@@ -21,4 +21,4 @@ PRODUCT_MODEL := Nexus 6
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
     BUILD_FINGERPRINT=google/shamu/shamu:5.0.1/LRX22C/1602158:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 5.0.1 LRX22C 1602158 release-keys"
+    PRIVATE_BUILD_DESC="Onyx" \
